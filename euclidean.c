75fa0df4a7c302a3d1502eb776a2d5e039f57f4f
Ahmed Muhammed Galadima <leapollo2005@gmail.com>
2021-03-10 09:31:50 +0300

    Added comment and pseoudoce

--- euclidean.c
+++ euclidean.c
@@ -4,11 +4,13 @@ int euclide(int n1, int n2);
 
 int main(int argc, char** argv)
 {
-    printf("%i is the gcd of 328 and 288\n",euclide(11,25));
+    printf("%i\n",euclide(26,26));
 }
 
 int euclide(int n1, int n2)
 {
+    int GCF = 0;
+
     while (n1 != n2)
     {
         if (n1 > n2)
@@ -16,10 +18,7 @@ int euclide(int n1, int n2)
         else
             n2 = n2 - n1;
     }
-
-    // gcf(greatest common factor) can either be equal to n1 or n2 since they will end up with same values.
-
-    int GCF = n1;
-
-    return GCF;
+    
+    // if the two numbers are equal it returns either one of the two back.  
+    GCF = n1;
 }
