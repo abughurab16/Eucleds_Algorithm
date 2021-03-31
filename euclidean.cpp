041589bea9260d5e4c27dd97124440a6640141c1
Ahmed Muhammed <leapollo2005@gmail.com>
2021-04-01 00:37:43 +0300

    Migrated to C++
    I finally decided to learn C++ and use it with Julia

--- euclidean.cpp
+++ euclidean.cpp
@@ -1,24 +1,30 @@
-#include <stdio.h>
+#include <iostream>
 
-int euclide(int n1, int n2);
+void euclide(int n1, int n2);
 
 int main(int argc, char** argv)
 {
-    printf("%i\n",euclide(26,26));
+   euclide(20,10);
 }
 
-int euclide(int n1, int n2)
+void euclide(int n1, int n2)
 {
     int GCF = 0;
-
+    int iterator = 0;
+    
     while (n1 != n2)
     {
         if (n1 > n2)
             n1 = n1 - n2;
         else
             n2 = n2 - n1;
+        
+        iterator = iterator + 1;
     }
     
     // if the two numbers are equal it returns either one of the two back.  
     GCF = n1;
+    
+    std::cout << "ran " << iterator << " times\n";
+    std::cout << "GCF ==> " << GCF << "\n";
 }
