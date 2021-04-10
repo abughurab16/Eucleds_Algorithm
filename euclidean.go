8c6b44d625f17504f2a5b10212984b9bc7bc303a
Ahmed Muhammed <leapollo2005@gmail.com>
2021-04-11 00:27:21 +0300

    Big Brain Time

--- euclidean.go
+++ euclidean.go
@@ -11,24 +11,31 @@ func abs(a int) {
    return a
 }
 
-func eucleids(n1, n2 int) int {
+func eucleidSub(n1, n2 int) int {
    n1 := abs(n1)
    n2 := abs(n2)
    
-   for {
-      // terminate early if they have equal values 
-      if n1 == n2 {
-         return n1
-      }
-      
-      if n1 > n2 {
-         n1 = n1 - n2
-      } else {
-         n2 = n2 - n1
-      }
-      
-   }
-   
+  // loop until both values are equal
+  for n1 != n2 {
+    
+    if n1 > n2 {
+      n1 = n1 - n2
+    } else {
+      n2 = n2 - n1
+    }
+    
+  }
    // either n1 or n2 will work :)
    return n1
+}
+
+func eucleidMod(n1, n2 int) int {
+  n1 := abs(n1)
+  n2 := abs(n2)
+  
+  for n2 != 0 {
+    a,b = b, a%b
+  }
+  
+  return a
 }
